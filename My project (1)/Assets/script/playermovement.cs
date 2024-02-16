b0ed25131d362ca168469acdfadddd70d41255e1
ElPilas1 <154227496+ElPilas1@users.noreply.github.com>
2024-02-16 13:01:39 +0100

    Velocidad
    a

---
+++ My project (1)/Assets/script/playermovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{   public float speed;
+    public KeyCode upkey;
+    public Vector2 dir;
+    public Vector2 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir =  Vector2.zero;
+        if (Input.GetKey(upkey) )
+        {
+            dir = new Vector2(0, 1);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        velocity =  speed * dir;
+    }
+
+
+
+
+
+
+}
+
+
+
